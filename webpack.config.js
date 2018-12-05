f62acbf003b847b56b9f3b04c37766d191d29b75
mimikria96 <chemiakina7493@mail.ru>
2018-12-05 14:34:18 +0300

    add start script

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,6 @@ module.exports = {
 	entry: [
 		`${__dirname}/src/index.js`,
 	],
-	output: {
-		path: __dirname,
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
 	externals: {
 		gon: 'gon',
 	},
