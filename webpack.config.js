916bcb098abbefd483326629488dea2b82f14a47
mimikria96 <chemiakina7493@mail.ru>
2018-12-05 12:44:45 +0300

    change webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ module.exports = {
 	entry: [
 		`${__dirname}/src/index.js`,
 	],
+	output: {
+		path: __dirname,
+		publicPath: '/',
+		filename: 'bundle.js'
+	},
 	externals: {
 		gon: 'gon',
 	},
